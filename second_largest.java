16f63f4f38bc9f53e00c8b62ed615af9700a710a
Glen Jayson Dmello <120797275+glenjaysondmello@users.noreply.github.com>
2025-08-07 20:12:48 +0530

    Create second_largest.java

---
+++ second_largest.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        int maxEle = getMax(arr);
+        
+        int sLargest = -1;
+        
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != maxEle && arr[i] > sLargest) sLargest = arr[i];
+        }
+        
+        return sLargest;
+    }
+    
+    static int getMax(int[] arr) {
+        int max = arr[0];
+        
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] > max) max = arr[i];
+        }
+        
+        return max;
+    }
+}
+
+
+// Examples:
+
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
+
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
+
+// Input: arr[] = [10, 10, 10]
+// Output: -1
+// Explanation: The largest element of the array is 10 and the second largest element does not exist.
+
